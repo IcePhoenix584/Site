a3cce4a1fa00b7aa17b2f433c93fcbece960b280
IcePhoenix584 <max12by12@gmail.com>
2018-07-19 21:12:46 -0400

    Added the files

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+/*  $(".side1").click(function(){
+    $(".orange").css("top", "100%");
+    $(".orange").fadeOut(1000);
+  })
+*/
+  $(".orange").hide();
+
+  $(".side1").click(function(){
+$(".orange").css("top","-100%");
+    //$(".orange").css("top","0%");
+    //$(".orange").fadeIn(1000);
+  })
+
+});
